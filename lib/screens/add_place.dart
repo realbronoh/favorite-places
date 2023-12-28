ee95576907cd7a5817185105754de4622fb275c5
realbro <realbro.noh@gmail.com>
2023-12-28 23:04:27 +0900

    location lib setup

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -62,6 +63,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ImageInput(
               onPickImage: _selectImage,
             ),
+            const SizedBox(height: 10),
+            const LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  Location? _pickedLocation;
+  bool _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+    setState(() {
+      _isGettingLocation = false;
+    });
+    print(locationData.latitude);
+    print(locationData.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No Location Chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+    );
+
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          height: 170,
+          width: double.infinity,
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get Current Location'),
+              onPressed: _getCurrentLocation,
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.map),
+              label: const Text('Select on Map'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
